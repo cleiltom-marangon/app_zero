7f2444c55ecbc972690854f520c4a4c30b55142c
cleiltom-marangon <83768623+cleiltom-marangon@users.noreply.github.com>
2025-12-15 15:35:08 -0300

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
 
   const ok =
     password === user.password ||
-    (await bcrypt.compare(password, user.password));
+    (await bcrypt.compare);
 
   if (!ok) return res.status(401).json({ error: 'invalid' });
 
